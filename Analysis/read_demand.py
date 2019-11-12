af2bb980ffe85be5948aaddd844701b6fee4f695
Andy Jones <agjones6@ncsu.edu>
2019-11-12 10:10:51 -0500

    Created a file for reading in load Profiles

---
+++ Analysis/read_demand.py
@@ -0,0 +1,7 @@
+# This python script is made to read csv files from the eia
+
+# Website to get the information
+# https://www.eia.gov/beta/electricity/gridmonitor/dashboard/electric_overview/regional/REG-CAR
+
+import pandas as pd
+import numpy as np
